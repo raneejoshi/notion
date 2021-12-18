d1ce878e8045237959c5f54e878586c1ce1a87f1
raneejoshi <raneejoshi@gmail.com>
2021-12-18 22:41:34 +0800

    Update and rename f1_drivers_standings_workflow.py to f1_standings_workflow.py

--- f1_standings_workflow.py
+++ f1_standings_workflow.py
@@ -44,3 +44,36 @@ def get_drivers_standings():
     for index, row  in drivers_standings.iterrows():
         create_post_drivers_standings(row.driverID, row.position, row.positionText, row.points, row.wins, row.driver, row.nationality, row.constructor, row.constructorID)
 get_drivers_standings()
+
+CONSTRUCTORS_DATABASE_KEY = os.environ['CONSTRUCTORS_DATABASE_KEY']
+def create_post_constructors_standings(position, positionText, points, wins, nationality, name, constructorID):
+
+    headers = {
+        'Authorization': f"Bearer {F1_NOTION_API_KEY}",
+        'Content-Type': 'application/json',
+        'Notion-Version': '2021-08-16',
+    }
+   
+    data = { "parent": { "database_id": CONSTRUCTORS_DATABASE_KEY }, "properties": { 
+        "driver":  {"title": [ { "text": { "content":  name}} ] },
+        "position": { "number": int(position)},
+        "positionText":  {"rich_text": [{"text": {"content":  str(positionText)}}]},
+        "points": { "number": float(points)},
+        "wins": { "number": int(wins)},
+        "nationality": {"rich_text": [{"text": {"content": nationality}}]},
+        "constructorID":  {"rich_text": [{"text": {"content": constructorID}}]},
+    },  }
+        
+    c=requests.post('https://api.notion.com/v1/pages', headers=headers, json=data)
+    if c.status_code != 200:
+        raise RuntimeError(c.content)
+        print(data)
+        #i=i+1
+
+def get_constructors__standings():
+    constructors_standings=pyergast.constructor_standings()
+    #drivers_standings = drivers_standings.astype({"nationality": str, "driver": str})
+    for index, row  in constructors_standings.iterrows():
+        create_post_constructors_standings(row.name, row.position, row.positionText, row.points, row.wins, row.nationality, row.constructorID)
+
+get_constructors_standings()
